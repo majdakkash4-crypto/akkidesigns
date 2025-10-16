0fcafc4c6bd7da81430d58f3eeee8679c145fc53
majdakkash4-crypto <majdakkash4@gmail.com>
2025-10-16 05:31:19 +0200

    Create RobotSpline.tsx

---
+++ components/RobotSpline.tsx
@@ -0,0 +1,11 @@
+'use client';
+import Spline from '@splinetool/react-spline';
+
+export default function RobotSpline() {
+  return (
+    <Spline
+      scene="https://prod.spline.design/6W8bbrZqU6qKyXEL/scene.splinecode"
+      className="w-full h-full"
+    />
+  );
+}
